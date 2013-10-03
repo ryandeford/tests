3f64d17b5d8797ba22c304d4c94bb29f162aa5e4
RyanAdmin <ryandeford@gmail.com>
2013-10-03 13:54:54 -0400

    initial commit of default rails project template to be used with http://ruby.railstutorial.org
    ryanadmin@ubuntu:~$ rvm -v
    
    rvm 1.22.15 (stable) by Wayne E. Seguin <wayneeseguin@gmail.com>, Michal Papis <mpapis@gmail.com> [https://rvm.io/]
    
    ryanadmin@ubuntu:~$ ruby -v
    ruby 2.0.0p247 (2013-06-27 revision 41674) [x86_64-linux]
    ryanadmin@ubuntu:~$ rails -v
    Rails 4.0.0
    ryanadmin@ubuntu:~$ gem -v
    2.1.5

---
+++ tutorials/rails/ruby.railstutorial.org/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyRailstutorialOrg::Application.config.secret_key_base = 'c12f2c9508c9f14ea011a8bc33e60a5eb575ba9d7237643f0e3c65105545ca0bf3c1793539f86270a25053c8c27c6e309fcd4abb970349feb44b151f58d8e9a4'
