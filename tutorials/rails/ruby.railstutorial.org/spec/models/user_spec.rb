ef2e3331a96b5e1ef46258dfd2397644ac317273
RyanAdmin <ryandeford@gmail.com>
2013-10-07 13:23:17 -0400

    Finish sign in features

--- tutorials/rails/ruby.railstutorial.org/spec/models/user_spec.rb
+++ tutorials/rails/ruby.railstutorial.org/spec/models/user_spec.rb
@@ -11,11 +11,17 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:created_at) }
   it { should respond_to(:updated_at) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+
+  describe 'remember token' do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe 'when name is not present' do
     before { @user.name = ' ' }
